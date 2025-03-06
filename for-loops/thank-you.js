2da09ea36fb101f5ca714b5a7ed404fb9bb3380b
Chinemerem Ugbaja <marycynthiaconfirm@gmail.com>
2025-03-06 01:43:05 +0100

    modifies thank you.js

--- for-loops/thank-you.js
+++ for-loops/thank-you.js
@@ -1,6 +1,6 @@
 // Given an array of names, write a program that prints a thank your message for each of the user. Sample output: "Thank you user for the birthday gift", where "user" is the name(s) from the array
 
-let nameArray = ["Andrew", "Chi", "Kate", "Japan"];
+let nameArray = ["Andrew", "Chi", "Kate", "Japan", "kc"];
 
 for (let i = 0; i < nameArray.length; i++) {
   console.log(`Thank you ${nameArray[i]} for the birthday gift`);
